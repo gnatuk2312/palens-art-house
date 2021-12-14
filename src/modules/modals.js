151f1a8a18a5a5f452119b334a51c7c9e9b4ad36
gnatuk2312 <84677177+gnatuk2312@users.noreply.github.com>
2021-12-14 15:01:54 +0200

    Revert "bug fix modals"
    This reverts commit 1371a709619700e4369860223525951dbdb5f789.

--- src/modules/modals.js
+++ src/modules/modals.js
@@ -32,7 +32,6 @@ const modals = () => {
 				modal.style.display = 'block';
 				// Забирає скролл при відкритті модального вікна
 				document.body.style.overflow = 'hidden';
-				document.body.classList.add('overflow-hidden');
 				document.body.style.paddingRight = `${scroll}px`;
 				presentBtn.style.right = `${currentRightPresent + scroll}px`;
 				scrollBtn.style.display = "none";
@@ -53,7 +52,6 @@ const modals = () => {
 				modal.style.display = "none";
 				// Повертає скролл
 				document.body.style.overflow = "";
-				document.body.classList.remove('overflow-hidden');
 				document.body.style.paddingRight = `0px`;
 				presentBtn.style.right = `${currentRightPresent}px`;
 				scrollBtn.style.display = "block";
@@ -69,7 +67,6 @@ const modals = () => {
 			if (e.target === modal) {
 				modal.style.display = "none";
 				document.body.style.overflow = "";
-				document.body.classList.remove('overflow-hidden');
 				document.body.style.paddingRight = `0px`;
 				presentBtn.style.right = `${currentRightPresent}px`;
 				scrollBtn.style.display = "block";
